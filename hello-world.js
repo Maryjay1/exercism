678d6348600ddb033863dd93479271f40ac56e3d
Mary Joseph <mary_jay@icloud.com>
2019-12-04 22:42:54 +0000

    hello world javaScript

---
+++ hello-world.js
@@ -0,0 +1,15 @@
+function hello() {
+  console.log("hello World");
+}
+
+hello();
+//
+// This is only a SKELETON file for the 'Hello World' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const hello = () => {
+  throw new Error();
+};
+
+exercism submit /path/to/file [/path/to/file2 ...]
